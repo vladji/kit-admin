e50e6c360c04c6bc898d38e1c13dc83969ba4ac4
vladji <vladjislav@gmail.com>
2025-06-19 01:08:33 +0700

    base config

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+export default {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+  trailingComma: 'es5',
+  plugins: ["prettier-plugin-tailwindcss"],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import svgr from 'vite-plugin-svgr';
+import { resolve } from 'node:path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react({
+      babel: {
+        plugins: [
+          [
+            'formatjs',
+            {
+              idInterpolationPattern: '[sha512:contenthash:base64:6]',
+              ast: true,
+            },
+          ],
+        ],
+      },
+    }),
+    tailwindcss(),
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: {
+        exportType: 'default',
+      },
+      include: '**/*.svg',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+});
