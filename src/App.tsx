ab725b9c8d71062ef841b8603883cdde44b901c4
vladji <vladjislav@gmail.com>
2025-06-30 00:43:34 +0700

    feat: Toaster

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { ContextProvider } from 'app/providers/ContextProvider';
 import { LocaleProvider } from 'app/providers/LocaleProvider';
 import RouterProvider from 'app/providers/RouterProvider';
 import { TanStackQuery } from './app/providers/TanStackQuery';
+import { Toaster } from './shared/ui/Toaster';
 
 function App() {
   return (
     <ContextProvider>
       <LocaleProvider>
+        <Toaster position="top-right" richColors />
         <TanStackQuery>{RouterProvider}</TanStackQuery>
       </LocaleProvider>
     </ContextProvider>
